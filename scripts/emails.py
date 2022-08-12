868fd1afc937b31c60db82438cdd09dab87d0902
AbishaKugathasan <77181669+AbishaKugathasan@users.noreply.github.com>
2022-08-12 12:53:15 -0400

    Update emails.py

--- scripts/emails.py
+++ scripts/emails.py
@@ -1,3 +1,4 @@
 # Write the solution for "New Emails".
+import rm
 #Regex to find the '@gmail.com' part, so I can change it to mlh.io - only gets the valid emails as said in the prompt (@([a-zA-z])+\.com) 
 replace (@([a-zA-z])+\.com, @mlh.io)
