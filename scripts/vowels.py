a1c41d48e7e8aaa8a5ac51edb794907bc7cee2da
AbishaKugathasan <77181669+AbishaKugathasan@users.noreply.github.com>
2022-08-11 22:51:05 -0400

    Update vowels.py

--- scripts/vowels.py
+++ scripts/vowels.py
@@ -1,1 +1,17 @@
+import re 
+import os
+
+#convert the python file into a string 
+text = open("vowel.txt")
+    
 # Write the solution for "Filter Vowels".
+#Count the number of vowels 
+
+#find the vowels by using regex 
+vowels = (a|e|i|o|u)
+
+#count the vowels by storing it in an array 
+number_of_vowels = len(re.findall(vowels, text))
+
+#Change the name of the file to vowels-number.txt
+os.rename("vowel.txt", "vowels-"+number_of_vowels"+".txt"
