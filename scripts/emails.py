6b83afa942f488b0ceaf060f12c7779ae19d1170
AbishaKugathasan <77181669+AbishaKugathasan@users.noreply.github.com>
2022-08-15 18:20:15 -0400

    Update emails.py

--- scripts/emails.py
+++ scripts/emails.py
@@ -1,4 +1,7 @@
-# Write the solution for "New Emails".
-import rm
-#Regex to find the '@gmail.com' part, so I can change it to mlh.io - only gets the valid emails as said in the prompt (@([a-zA-z])+\.com) 
-replace (@([a-zA-z])+\.com, @mlh.io)
+import re
+from string import *
+print("opening file")
+
+with open ('emails.txt', 'r') as f:
+  context = f.read()
+  print(re.sub("@([a-zA-z])+\.com", "mlh.io",context))
